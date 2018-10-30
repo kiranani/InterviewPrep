745a4d47b9bc0294f19cf923a36f267004946ba7
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2018-10-29 22:48:56 -0400

    Create 0206_Reverse_Linked_List.py

---
+++ Python3/0206_Reverse_Linked_List.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None or head.next is None:
+            return head
+        else:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+            head.next = None
+            return newHead
+        
