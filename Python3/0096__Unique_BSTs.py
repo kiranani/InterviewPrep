a661256ff45d4d094c9df0a4e2add5cf5ceb7df5
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2018-10-31 22:20:21 -0400

    Create 0096__Unique_BSTs.py

---
+++ Python3/0096__Unique_BSTs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [1]  + [0] * n
+        for i in range(1, n + 1):
+            for j in range(1, i + 1):
+                dp[i] += dp[j - 1] * dp[i - j]
+        return dp[-1]
+        
