0780920c8c00808f2b05017312a9acc24527d1cb
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-09-03 23:08:02 -0700

    Update 0056_Merge_Intervals.py

--- Python3/0056_Merge_Intervals.py
+++ Python3/0056_Merge_Intervals.py
@@ -1,62 +1,13 @@
-# Definition for an interval.
-# class Interval:
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
-
 class Solution:
-    def merge(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: List[Interval]
-        """
-        if len(intervals) == 0:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
             return []
-        points = []
-        for point in intervals:
-            points.append([point.start, 0])
-            points.append([point.end, 1])
-        points.sort(key=operator.itemgetter(0))
-        s, ints, prev = [], [], points[0][0]
-        for point in points:
-            if point[1] == 0:
-                s.append(point[0])
-            else:
-                if len(s) == 1:
-                    start = s.pop()
-                    if len(ints) and start == ints[-1].end:
-                        ints[-1].end = point[0]
-                    else:
-                        ints.append(Interval(start, point[0]))
-                else:
-                    s.pop()
-        return ints
-
-    
-# Definition for an interval.
-# class Interval:
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
-
-class Solution:
-    def merge(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: List[Interval]
-        """
-        if len(intervals) == 0:
-            return []
-        def intersect(a, b):
-            return a.end >= b.start
-        intervals.sort(key=lambda x: (x.start, -x.end))
-        #print([(x.start, x.end) for x in intervals])
-        ints = [intervals[0]]
+        intervals.sort(key=lambda x: (x[0], x[1]))
+        ans = [intervals[0]]
         for interval in intervals[1:]:
-            #print(interval.start, interval.end, ints[-1].start, ints[-1].end)
-            if intersect(ints[-1], interval):
-                ints[-1].end = max(ints[-1].end, interval.end)
+            if interval[0] <= ans[-1][1]:
+                ans[-1][1] = max(interval[1], ans[-1][1])
             else:
-                ints.append(interval)
-        return ints
-                    
+                ans.append(interval) 
+        return ans
+    
