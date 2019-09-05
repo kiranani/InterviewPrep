d9ec7c26858897cc8f60f94a71638e9b9868edc1
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-09-04 23:24:43 -0700

    Update 0200_Number_Of_Islands.py

--- Python3/0200_Number_Of_Islands.py
+++ Python3/0200_Number_Of_Islands.py
@@ -1,26 +1,21 @@
 class Solution:
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        m = len(grid)
-        if m == 0:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
             return 0
-        n, c = len(grid[0]), 0
+        r, c = len(grid), len(grid[0])
         def dfs(i, j):
-            if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] == "0":
-                return
-            else:
+            if i > -1 and i < r and j > -1 and j < c and grid[i][j] == "1":
                 grid[i][j] = "0"
-                dfs(i, j + 1)
-                dfs(i + 1, j)
-                dfs(i - 1, j)
                 dfs(i, j - 1)
-        for i in range(m):
-            for j in range(n):
+                dfs(i, j + 1)
+                dfs(i - 1, j)
+                dfs(i + 1, j)
+        islands = 0
+        for i in range(r):
+            for j in range(c):
                 if grid[i][j] == "1":
-                    c += 1
+                    islands += 1
                     dfs(i, j)
-        #print(grid)
-        return c
+        return islands
+                
+        
