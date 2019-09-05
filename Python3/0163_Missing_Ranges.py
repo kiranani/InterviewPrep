2518d199d35f12283f00b1b111b937d5c05ce738
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-09-04 23:02:38 -0700

    Update 0163_Missing_Ranges.py

--- Python3/0163_Missing_Ranges.py
+++ Python3/0163_Missing_Ranges.py
@@ -1,25 +1,17 @@
 class Solution:
-    def findMissingRanges(self, nums, lower, upper):
-        """
-        :type nums: List[int]
-        :type lower: int
-        :type upper: int
-        :rtype: List[str]
-        """
-        i, n, ans = 0, len(nums), []
-        while i < n:
-            #print(lower)
-            start = i
-            while i < n - 1 and nums[i] + 1 == nums[i + 1]:
-                i += 1
-            if lower + 1 == nums[start]:
-                ans.append(str(lower))
-            elif lower < nums[start] - 1:
-                ans.append(str(lower) + "->" + str(nums[start] - 1))
-            lower = nums[i] + 1
-            i += 1
-        if lower == upper:
-            ans.append(str(lower))
-        elif lower < upper:
-            ans.append(str(lower) + "->" + str(upper))
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
+        ans, l = [], lower
+        def updateAns(l, r, ans):
+            if l == r:
+                ans.append(str(l))
+            elif l < r:
+                ans.append(str(l) + "->" + str(r))
+        
+        for num in nums:
+            if l < num:
+                r = num - 1
+                updateAns(l, r, ans)
+            l = num + 1
+        updateAns(l, upper, ans)
         return ans
+        
