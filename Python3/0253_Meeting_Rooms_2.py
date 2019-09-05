c24a73a27c7e14c5b5f409a9c5018978bd872500
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-09-05 01:16:50 -0700

    Update 0253_Meeting_Rooms_2.py

--- Python3/0253_Meeting_Rooms_2.py
+++ Python3/0253_Meeting_Rooms_2.py
@@ -1,23 +1,12 @@
-# Definition for an interval.
-# class Interval:
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
-
 class Solution:
-    def minMeetingRooms(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: int
-        """
-        if len(intervals) == 0:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if not intervals:
             return 0
-        intervals.sort(key=lambda x: x.start)
-        rooms = [intervals[0].end]
+        intervals.sort(key=lambda x: (x[0], x[1]))
+        h = [intervals[0][1]]
         for interval in intervals[1:]:
-            if interval.start >= rooms[0]:
-                heapq.heappushpop(rooms, interval.end)
-            else:
-                heapq.heappush(rooms, interval.end)
-        return len(rooms)
-        
+            if h[0] <= interval[0]:
+                heapq.heappop(h)
+            heapq.heappush(h, interval[1])
+        return len(h)
+            
