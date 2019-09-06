c94e414aa23351d03bff42fbf9a51c60a2cd26d4
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-09-05 18:48:45 -0700

    Update 0359_Logger_Rate_Limiter.py

--- Python3/0359_Logger_Rate_Limiter.py
+++ Python3/0359_Logger_Rate_Limiter.py
@@ -4,29 +4,29 @@ class Logger:
         """
         Initialize your data structure here.
         """
-        self.h = {}
+        self.d = {}
+        self.h = []
         
 
-    def shouldPrintMessage(self, timestamp, message):
+    def truncateLogs(self, time):
+        while self.h and self.h[0][0] < time:
+            m = heapq.heappop(self.h)[1]
+            if self.d[m] < time:
+                del self.d[m]
+        
+        
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
         """
         Returns true if the message should be printed in the given timestamp, otherwise returns false.
         If this method returns false, the message will not be printed.
         The timestamp is in seconds granularity.
-        :type timestamp: int
-        :type message: str
-        :rtype: bool
         """
-        x = self.h.get(message)
-        if x is None:
-            self.h[message] = timestamp
-            return True
-        else:
-            if timestamp < x + 10:
-                return False
-            else:
-                self.h[message] = timestamp
-                return True
-        
+        printMessage = timestamp > self.d.get(message, -1)
+        if printMessage:
+            self.d[message] = timestamp + 9
+            heapq.heappush(self.h, (timestamp + 9, message))
+        self.truncateLogs(timestamp)
+        return printMessage
 
 
 # Your Logger object will be instantiated and called as such:
