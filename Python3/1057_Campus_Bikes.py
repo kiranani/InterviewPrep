c5937daa047e3ac3aef64f95512375aa6c5e44b0
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-09-08 22:26:50 -0700

    Update 1057_Campus_Bikes.py

--- Python3/1057_Campus_Bikes.py
+++ Python3/1057_Campus_Bikes.py
@@ -6,13 +6,15 @@ class Solution:
         d, h = [], [[] for _ in range(n)]
         for i, w in enumerate(workers):
             for j, b in enumerate(bikes):
-                heapq.heappush(h[i], (abs(b[0] - w[0]) + abs(b[1] - w[1]), i, j))
-            heapq.heappush(d, heapq.heappop(h[i]))
+                heapq.heappush(h[i], (abs(b[0] - w[0]) + abs(b[1] - w[1]), j))
+            x, b = heapq.heappop(h[i])
+            heapq.heappush(d, (x, i, b))
         ans, bs = [None] * n, set()
         while len(bs) < n:
             x, w, b = heapq.heappop(d)
             if b in bs:
-                heapq.heappush(d, heapq.heappop(h[w]))
+                x, b = heapq.heappop(h[w])
+                heapq.heappush(d, (x, w, b))
             else:
                 bs.add(b)
                 ans[w] = b
