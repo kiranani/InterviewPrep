ac14ea1e2632cb2e6854446bb12f9de83da5a8dd
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-09-15 12:57:24 -0700

    Create 0708_Insert_Into_A_Cyclic_Sorted_List.py

---
+++ Python3/0708_Insert_Into_A_Cyclic_Sorted_List.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, next):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    def insert(self, head: 'Node', insertVal: int) -> 'Node':
+        if not head:
+            return Node(insertVal, None)
+        root = head
+        while True:
+            if (root.next == head 
+                or root.val <= insertVal <= root.next.val 
+                or root.next.val < root.val 
+                and (insertVal < root.next.val or insertVal > root.val)):
+                root.next = Node(insertVal, root.next)
+                break
+            root = root.next
+        return head
+        
