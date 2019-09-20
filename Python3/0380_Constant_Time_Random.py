30a338b19a9770d171213e23335e245ed5b3b2f4
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-09-19 18:50:46 -0700

    Update 0380_Constant_Time_Random.py

--- Python3/0380_Constant_Time_Random.py
+++ Python3/0380_Constant_Time_Random.py
@@ -1,50 +1,43 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.count = 0
-        self.hash = {}
-        self.array = []
+        """
+        Initialize your data structure here.
+        """
+        self.h = {}
+        self.l = []
         
 
-    def insert(self, val):
+    def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        :type val: int
-        :rtype: bool
         """
-        if self.hash.get(val) is not None:
+        if self.h.get(val) is not None:
             return False
-        else:
-            self.hash[val] = self.count
-            self.array.append(val)
-            self.count += 1
-            return True
+        self.h[val] = len(self.l)
+        self.l.append(val)
+        return True
         
 
-    def remove(self, val):
+    def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
-        :type val: int
-        :rtype: bool
         """
-        #print(self.array, val, self.hash.get(val))
-        if self.hash.get(val) is not None:
-            self.hash[self.array[-1]], self.array[self.hash[val]], self.array[-1] = self.hash[val], self.array[-1], self.array[self.hash[val]]
-            del self.hash[val]
-            self.array.pop()
-            self.count -= 1
-            return True
-        else:
-            return False         
+        if not self.h or self.h.get(val) is None:
+            return False
+        i = self.h[val]
+        self.h[self.l[-1]] = i
+        del self.h[val]
+        self.l[i] = self.l[-1]
+        self.l.pop()
+        return True
         
 
-    def getRandom(self):
+    def getRandom(self) -> int:
         """
         Get a random element from the set.
-        :rtype: int
         """
-        return self.array[random.randint(0, self.count - 1)]
-        
+        return len(self.l) and self.l[random.randint(0, len(self.l) - 1)]
         
 
 
