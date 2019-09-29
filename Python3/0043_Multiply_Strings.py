6665d830886446065bf3bc24eb7cca9abee3ae40
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-09-28 20:11:43 -0700

    Create 0043_Multiply_Strings.py

---
+++ Python3/0043_Multiply_Strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        n1, n2 = len(num1), len(num2)
+        n, num1, num2 = n1 + n2, [int(x) for x in num1], [int(x) for x in num2]
+        q = [0] * n
+        for i in range(n1):
+            c = 0
+            for j in range(n2):
+                c, q[~(i + j)] = divmod(c + q[~(i + j)] + num1[~i] * num2[~j], 10)
+            q[~(i + j + 1)] = c
+        i = 0
+        while i < n and q[i] == 0:
+            i += 1
+        ans = "".join([str(x) for x in q[i:]])
+        if ans:
+            return ans
+        return "0"
+        
