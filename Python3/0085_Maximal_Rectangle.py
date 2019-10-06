85b606b9da05c5a1281c7eb3d18ecc6f9a8b2a2c
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-10-06 14:08:20 -0700

    Update 0085_Maximal_Rectangle.py

--- Python3/0085_Maximal_Rectangle.py
+++ Python3/0085_Maximal_Rectangle.py
@@ -1,35 +1,22 @@
 class Solution:
-    def maximalRectangle(self, matrix):
-        """
-        :type matrix: List[List[str]]
-        :rtype: int
-        """
-        m = len(matrix)
-        if m == 0:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix:
             return 0
-        n = len(matrix[0])
-        def findMax(row, index):
-            if row[index] == 0:
-                return 0
-            else:
-                s, i = row[index], index - 1
-                while i > -1 and row[i] >= row[index]:
-                    s += row[index]
-                    i -= 1
-                i = index + 1
-                while i < n and row[i] >= row[index]:
-                    s += row[index]
-                    i += 1
-                return s
-        ans, accu = 0, [0] * n
+        m, n = len(matrix), len(matrix[0])
+        mx, dp = 0, [0] * n
         for row in matrix:
+            ndp = [0] * n
             for j in range(n):
-                if row[j] == "0":
-                    accu[j] = 0
-                else:
-                    accu[j] += 1
-            #print(accu)
-            for j in range(n):
-                s = findMax(accu, j)
-                ans = ans if ans > s else s
-        return ans
+                if row[j] == "1":
+                    ndp[j] = dp[j] + 1
+            dp = ndp
+            s = [-1]
+            for i, e in enumerate(dp):
+                while s[-1] != -1 and e <= dp[s[-1]]:
+                    mx = max(mx, dp[s.pop()] * (i - s[-1] - 1))
+                s.append(i)
+            while s[-1] != -1:
+                mx = max(mx, dp[s.pop()] * (n - s[-1] - 1))
+        return mx
+            
+        
