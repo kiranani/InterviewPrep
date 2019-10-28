b63eef80a6a300fd84cca039f902bb270d741d3d
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-10-28 09:54:37 -0700

    Update 0636_Exclusive_Time_Of_Functions.py

--- Python3/0636_Exclusive_Time_Of_Functions.py
+++ Python3/0636_Exclusive_Time_Of_Functions.py
@@ -1,25 +1,18 @@
 class Solution:
-    def exclusiveTime(self, n, logs):
-        """
-        :type n: int
-        :type logs: List[str]
-        :rtype: List[int]
-        """
-        if not n:
-            return []
-        ans, stk = [0] * n, []
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        ans, stk = [0] * (n + 1), [[-1, 0]]
         for log in logs:
-            i, se, t = log.split(":")
-            i, t = int(i), int(t)
+            f, se, t = log.split(":")
+            f, t = int(f), int(t)
             if se == "start":
-                if stk:
-                    ans[stk[-1][0]] += t - stk[-1][1]
-                    stk[-1][1] = t - stk[-1][1]
-                stk.append([i, t])
+                ans[stk[-1][0]] += t - stk[-1][1]
+                stk.append([f, t])
             else:
-                e = stk.pop()
-                if stk:
-                    stk[-1][1] = t + 1
-                ans[e[0]] += t + 1 - e[1]
+                t += 1
+                ans[stk[-1][0]] += t - stk[-1][1]
+                stk.pop()
+                stk[-1][-1] = t
+        ans.pop()
         return ans
+                    
         
