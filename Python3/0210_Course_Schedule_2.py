6630ad396a94bca1533b8fe25415136a09fafe00
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-11-07 09:10:11 -0800

    Update 0210_Course_Schedule_2.py

--- Python3/0210_Course_Schedule_2.py
+++ Python3/0210_Course_Schedule_2.py
@@ -1,34 +1,21 @@
+from collections import defaultdict
 class Solution:
-    def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
-        if not numCourses:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph, deg = defaultdict(set), defaultdict(int)
+        for course, prereq in prerequisites:
+            graph[prereq].add(course)
+            deg[course] += 1
+        i, ans = 0, [None] * numCourses
+        deg_0 = [i for i in range(numCourses) if i not in deg]
+        while deg_0:
+            n = deg_0.pop()
+            ans[i], i = n, i + 1
+            for c in graph[n]:
+                deg[c] -= 1
+                if deg[c] == 0:
+                    deg_0.append(c)
+        if i < numCourses:
             return []
-        graph = [[] for i in range(numCourses)]
-        for edge in prerequisites:
-            graph[edge[0]].append(edge[1])
-        visited, courses = [0] * numCourses, []
-
-        def dfs(course):
-            if visited[course] == 1:
-                return False
-            elif visited[course] == 2:
-                return True
-            else:
-                visited[course] += 1
-                for node in graph[course]:
-                    if not dfs(node):
-                        return False
-                visited[course] += 1
-                courses.append(course)
-                return True
-
-        for i in range(numCourses):
-            if not dfs(i):
-                return []
-        return courses
-                
+        return ans
+                    
         
