cd06c4041d053b7b73702d770a257592ba051ec0
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2019-12-24 04:56:30 -0800

    Create 0367_Valid_Perfect_Square.py

---
+++ Python3/0367_Valid_Perfect_Square.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num < 2:
+            return True
+        left, right = 2, num // 2
+        while left <= right:
+            mid = (left + right) // 2
+            sqr = mid ** 2
+            if sqr == num:
+                return True
+            elif sqr < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+        
