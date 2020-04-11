87110cc9efce28fd7bcae990cbebf0547a1ce132
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2020-04-11 12:23:12 -0700

    Update 0844_Backspace_String_Compare.py

--- Python3/0844_Backspace_String_Compare.py
+++ Python3/0844_Backspace_String_Compare.py
@@ -1,11 +1,23 @@
 class Solution:
     def backspaceCompare(self, S: str, T: str) -> bool:
-        def get_str(s):
-            a = []
-            for c in s:
-                if c != "#":
-                    a.append(c)
-                elif a:
-                    a.pop()
-            return "".join(a)
-        return get_str(S) == get_str(T)
+        def get_char(s, i):
+            hashes, chars, c = 0, 0, ""
+            while i > -1:
+                c = s[i]
+                if c == "#":
+                    hashes += 1
+                else:
+                    chars += 1
+                i -= 1
+                if chars - hashes == 1:
+                    return i, c
+            return -1, ""
+        
+        i, j = len(S) - 1, len(T) - 1
+        while i > -1 or j > -1:
+            (i, cs), (j, ct) = get_char(S, i), get_char(T, j)
+            if cs != ct:
+                return False
+        return True
+                
+        
