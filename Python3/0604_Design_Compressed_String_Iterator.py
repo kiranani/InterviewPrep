d7ea9a24323aa8b9e86c3f251c4cb3cdbacf883d
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2020-04-15 23:47:41 -0700

    Update 0604_Design_Compressed_String_Iterator.py

--- Python3/0604_Design_Compressed_String_Iterator.py
+++ Python3/0604_Design_Compressed_String_Iterator.py
@@ -1,29 +1,31 @@
 class StringIterator:
 
     def __init__(self, compressedString: str):
-        self.counts = []
-        i, n = 0, len(compressedString)
-        while i < n:
-            ch, c = compressedString[i], 0
-            i += 1
-            while i < n and compressedString[i].isdigit():
-                c = 10 * c + int(compressedString[i])
-                i += 1
-            self.counts.append([ch, c])
-        self.i = 0
+        self.s, self.i, self.n = compressedString, 0, len(compressedString)
+        self.ch, self.c = self.compute()
         
-
-    def next(self) -> str:
-        ch = " "
-        if self.hasNext():
-            self.counts[self.i][1] -= 1
-            ch = self.counts[self.i][0]
-            if self.counts[self.i][1] == 0:
+        
+    def compute(self):
+        ch, c = " ", 0
+        if self.i < self.n:
+            ch = self.s[self.i]
+            self.i += 1
+            while self.i < self.n and self.s[self.i].isdigit():
+                c = 10 * c + int(self.s[self.i])
                 self.i += 1
+        return ch, c
+        
+        
+    def next(self) -> str:
+        ch = self.ch
+        if self.hasNext():
+            self.c -= 1
+            if not self.c:
+                self.ch, self.c = self.compute()
         return ch
 
     def hasNext(self) -> bool:
-        return self.counts[-1][-1]
+        return self.ch != " "
 
 
 # Your StringIterator object will be instantiated and called as such:
