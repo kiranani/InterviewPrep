6093a811ae95f6c8536c79423c083c3dcf476544
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2020-04-18 14:29:10 -0700

    Update 0064_Minimum_Path_Sum.py

--- Python3/0064_Minimum_Path_Sum.py
+++ Python3/0064_Minimum_Path_Sum.py
@@ -1,17 +1,11 @@
 class Solution:
-    def minPathSum(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
+    def minPathSum(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        state = [x for x in grid[0]]
-        for i in range(1, n):
-            state[i] += state[i - 1]
         for i in range(1, m):
-            newState = [grid[i][0] + state[0]] + [0] * (n - 1)
+            grid[i][0] += grid[i - 1][0]
+        for j in range(1, n):
+            grid[0][j] += grid[0][j - 1]
+        for i in range(1, m):
             for j in range(1, n):
-                newState[j] = grid[i][j] + min(newState[j - 1], state[j])
-            state = newState
-        return state[-1]
-        
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+        return grid[-1][-1]
