847b1a0de8c40a33fc0a3c44f88f9514a30f88b8
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2020-05-10 15:00:33 -0700

    Update 0042_Trapping_Water.py

--- Python3/0042_Trapping_Water.py
+++ Python3/0042_Trapping_Water.py
@@ -1,12 +1,18 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        total, acc, high, high_i = 0, 0, 0, -1
+        total, high, high_i = 0, 0, -1
         for i, h in enumerate(height):
+            if h > high:
+                high, high_i = h, i
+                
+        acc, high = 0, 0
+        for h in height[:high_i + 1]:
             if h >= high:
                 total += acc
-                acc, high, high_i = 0, h, i
+                acc, high = 0, h
             else:
                 acc += high - h
+                
         acc, high = 0, 0
         for h in height[high_i:][::-1]:
             if h >= high:
@@ -15,4 +21,4 @@ class Solution:
             else:
                 acc += high - h
         return total
-        
+    
