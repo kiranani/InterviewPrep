7abfa3a51c0136922f6831f18ea6b9672f748346
Shardul Natu <43422294+kiranani@users.noreply.github.com>
2020-05-17 10:39:29 -0700

    Update 0096__Unique_BSTs.py

--- Python3/0096__Unique_BSTs.py
+++ Python3/0096__Unique_BSTs.py
@@ -1,12 +1,12 @@
 class Solution:
-    def numTrees(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        dp = [1]  + [0] * n
-        for i in range(1, n + 1):
-            for j in range(1, i + 1):
-                dp[i] += dp[j - 1] * dp[i - j]
-        return dp[-1]
+    def numTrees(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 1, 1
+        for i in range(2, n + 1):
+            iby2 = i // 2
+            for j in range(iby2):
+                dp[i] += 2 * dp[j] * dp[i - j - 1]
+            if i & 1:
+                dp[i] += dp[iby2] ** 2
+        return dp[n]
         
